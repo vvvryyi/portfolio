5297842e05ad4eb36074cfb415db22de66d0a08c
vvvryyi <124810205+vvvryyi@users.noreply.github.com>
2024-11-08 19:01:40 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 document.querySelector('.burger-menu').addEventListener('click', function() {
+    this.classList.toggle('active');
     document.querySelector('.nav__links').classList.toggle('active');
+}); 
+
+document.querySelectorAll('.nav__links a').forEach(link => {
+    link.addEventListener('click', () => {
+        document.querySelector('.burger-menu').classList.remove('active');
+        document.querySelector('.nav__links').classList.remove('active');
+    });
 }); 
